6e452be9368d744051bfbb05365a1e662ebd2a1b
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-04-03 12:11:48 -0300

    fix: Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,10 +3,19 @@ import './Button.css'
 
 export default props => {
 
-
-return(
-    <button className="button">{props.label}</button>
+    let classes = 'button '
     
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return (
+        <button 
+        onClick = { e => props.click && props.click(props.label)}
+        className={classes}>
+            {props.label}
+        </button>
+
     )
 
 }
